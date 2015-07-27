b07b83b4ce40ca23ab7e4c560f80051449fa4239
albarral <albarral@migtron.com>
2015-07-27 21:49:12 +0200

    samMemo DB: new fields
    TAB_PLACES.reward & TAB_CONNECTIONS.Q

--- db/schema_samMemo.sql
+++ db/schema_samMemo.sql
@@ -56,3 +56,55 @@ ALTER TABLE TAB_CONNECTIONS
 ADD CONSTRAINT PK_CONNECTIONS PRIMARY KEY (envID, placeID, connID);
 
 
+
+
+
+-- -----------------------------------------------------
+-- Table `TAB_TASKS`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TAB_TASKS` ;
+
+CREATE TABLE IF NOT EXISTS `TAB_TASKS` 
+(
+  `taskID` INT NOT NULL,
+  `description` VARCHAR(30) NULL,
+  `type` INT NULL
+);
+
+
+-- -----------------------------------------------------
+-- Table `TAB_STATES`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TAB_STATES` ;
+
+CREATE TABLE IF NOT EXISTS `TAB_STATES` 
+(
+  `taskID` INT NOT NULL,
+  `stateID` INT NOT NULL,
+  `description` VARCHAR(30) NULL,
+  `reward` FLOAT NULL
+);
+
+ALTER TABLE TAB_STATES
+ADD CONSTRAINT PK_STATES PRIMARY KEY (taskID, stateID);
+
+-- -----------------------------------------------------
+-- Table `TAB_TRANSITIONS`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TAB_TRANSITIONS` ;
+
+CREATE TABLE IF NOT EXISTS `TAB_TRANSITIONS` 
+(
+  `taskID` INT NOT NULL,
+  `stateID` INT NOT NULL,
+  `transID` INT NOT NULL,
+  `description` VARCHAR(30) NULL,
+  `nextState` INT NULL,
+  `cost` FLOAT NULL,
+  `Q` FLOAT NULL
+);
+
+ALTER TABLE TAB_STATES
+ADD CONSTRAINT PK_TRANSITIONS PRIMARY KEY (taskID, stateID, transID);
+
+
