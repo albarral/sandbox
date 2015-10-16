5cca5b68fb4f68ad613763994f67c671b2a04ddd
Ainoa <ainoa@migtron.com>
2015-10-16 12:06:12 +0200

    Changes on database files to have a good schema and update

--- db/update_samMemo.sql
+++ db/update_samMemo.sql
@@ -24,3 +24,40 @@ CREATE TABLE IF NOT EXISTS TAB_BOARD
 
 ALTER TABLE TAB_BOARD
 ADD CONSTRAINT PK_BOARD PRIMARY KEY (tryID);
+
+-- -----------------------------------------------------
+-- Table TAB_STATES
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS TAB_STATES ;
+
+CREATE TABLE IF NOT EXISTS `TAB_STATES` 
+(
+  taskID INT NOT NULL,
+  stateID INT NOT NULL,
+  description VARCHAR(30) NULL,
+  reward FLOAT NULL,
+  rewardDefense FLOAT NULL
+);
+
+ALTER TABLE TAB_STATES
+ADD CONSTRAINT PK_STATES PRIMARY KEY (taskID, stateID);
+
+-- -----------------------------------------------------
+-- Table TAB_TRANSITIONS
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS TAB_TRANSITIONS;
+
+CREATE TABLE IF NOT EXISTS TAB_TRANSITIONS 
+(
+  taskID INT NOT NULL,
+  stateID INT NOT NULL,
+  transID INT NOT NULL,
+  description VARCHAR(30) NULL,
+  nextState INT NULL,
+  cost FLOAT NULL,
+  Q FLOAT NULL,
+  QDefend FLOAT NULL
+);
+
+ALTER TABLE TAB_STATES
+ADD CONSTRAINT PK_TRANSITIONS PRIMARY KEY (taskID, stateID, transID);
